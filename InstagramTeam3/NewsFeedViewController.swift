8f6aa14a5fe409d6b1554000536b722922cdda20
Rui Ong <ruiong@Ruis-MacBook-Pro.local>
2017-02-10 17:58:07 +0800

    pod install

---
+++ InstagramTeam3/NewsFeedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramTeam3
+//
+//  Created by Rui Ong on 10/02/2017.
+//  Copyright © 2017 Rui Ong. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        
+    }
+
+    
+    
+    
+
+}
+
